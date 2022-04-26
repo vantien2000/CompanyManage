f9be7dce1fb2de570a26ad2bd6385ae6ee9a72d2
tiennguyenvan <tien.nguyenvan1@vti.com.vn>
2022-04-26 10:35:49 +0700

    update gem file for heroku

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,22 +4,14 @@ class LoginController < ApplicationController
   end
 
   def postLogin
-    result_params = validate_login params
-    render json: result_params
-    # validateLogin = User.new({'email': params[:email], 'password_digest': params[:password]})
-    # if validateLogin.invalid?
-    #   flash[:email] = validateLogin.errors[:email] ? validateLogin.errors[:email] : ''
-    #   flash[:password] = validateLogin.errors[:password] ? validateLogin.errors[:password] : ''
-    #   render "new"
-    # else
-    #   user = User.find_by_email(params[:email])
-    #   if user && user.authenticate(params[:password])
-    #     session[:user_id] = user.user_id
-    #     redirect_to '/'
-    #   end
-    #   render json: "aaa"
-    #   # flash[:error] = "Account Invalid! Please check your email and password"
-    # end
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      redirect_to "/"
+    else
+    flash[:error] = "Account Invalid! Please check your email and password"
+    redirect_to "/login"
+    end
   end
 
   private
--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,29 +1,14 @@
 module LoginHelper
-    # $validator = {
-    #     :email => {
-    #         { 'required': '', 'messages': 'Email is required!'},
-    #         { max: 256, messages: 'Length Max is 256!' },
-    #         { with: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,messages: 'Email is format!'}
-    #     }
-    # }
-    $validator = {
-        'email_required' => {'required' => '', 'messages' => 'Email is required!'},
-        'email_max'=> {'max' => 256, 'messages' => 'Length Max is 256!'},
-        # 'email_regex'=> {'regex'=> /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'messages'=> 'Email is format!'}
-    }
-    def validate_login params
-        params.select{ |k, v| puts v if k === 'email'}
-    end
-
-    # def checkRequired key, value
-    #     if value == ''
-    #         return key + '_' + 'required'
+    # def validate_login params
+    #     mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    #     if params[:email] == ""
+    #         flash[:email] = "Email is required!"
     #     end
-    # end
-
-    # def checkMax key, value, max
-    #     if value.length > max
-    #         return key+'_max'
+    #     if params[:email].length > 255)
+    #         flash[:email] = "Length Max is 255!"
+    #     end
+    #     if params[:email].match(mail_format).nil?
+    #         flash[:email] = "Email is format!"
     #     end
     # end
 end
